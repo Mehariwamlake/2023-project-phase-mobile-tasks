356de8bd0fd4c31a0b673e97143573ca18ebef83
Your Name <you@example.com>
2023-10-07 23:51:24 +0300

    bloc

---
+++ on-boarding/starter_project/starter/lib/features/article/presentation/bloc/article_bloc.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:starter/core/error/fail.dart';
+import 'package:starter/features/article/domain/entites/article.dart';
+import 'package:starter/features/article/domain/usecases/aticle_usecase.dart';
+
+part 'article_event.dart';
+part 'article_state.dart';
+
+class ArticleBloc extends Bloc<ArticleEvent, ArticleState> {
+  final DeletArticle deletArticle;
+  final EditArticle editArticle;
+  final PostArticle postArticle;
+  final GetArticle getArticle;
+  final GetArticleById getArticleById;
+  final GetArticleByUserId getArticleByUserId;
+
+
+   ArticleState articleSucessOrFail(Either<Failure, Article> data) {
+    return data.fold(
+      (failure) => ArticleFailerState(error: failure),
+      (success) => ArticleSuccessState(article: success));
+  }
+
+  void _getArticleById(
+    GetArticleByIdEvent event, Emitter<ArticleState> emit) async {
+      emit(ArticleLoadingState());
+      final result = await getArticleById(event.articleId);
+      emit(articleSucessOrFail(result));
+    }
+
+
+  ArticleBloc({
+    required this.deletArticle,
+    required this.editArticle,
+    required this.getArticle,
+    required this.getArticleById,
+    required this.getArticleByUserId,
+    required this.postArticle,
+  }) : super(ArticleInitial()) {
+    on<ArticleEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
